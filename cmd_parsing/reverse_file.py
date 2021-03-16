29c0ce74478a3cd4c4b18503f4dcd0a2b43eadec
Stijn Nass <stijnnass@hotmail.com>
2021-03-16 10:57:09 +0100

    cmd_parsing

--- cmd_parsing/reverse_file.py
+++ cmd_parsing/reverse_file.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import argparse
+import sys
 
 parser = argparse.ArgumentParser(description='Read a file in reverse')
 parser.add_argument('filename', help='the file to read')
@@ -8,4 +9,20 @@ parser.add_argument('--version', '-v', action='version', version='%(prog)s 1.0')
 
 
 args = parser.parse_args()
-print(args)
+
+try:
+    f = open(args.filename)
+    limit = args.limit
+except FileNotFoundError as err:
+    print(f"Error: {err}")
+    sys.exit(1)
+else:
+    with f:
+        lines = f.readlines()
+        lines.reverse()
+
+    if limit:
+        lines = lines[:limit]
+
+    for line in lines:
+        print(line.strip()[::-1])
